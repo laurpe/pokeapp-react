bc4c141a988926edf4991308056f45e08cbb5683
laurpe <laura.perttila@edu.bc.fi>
2022-04-25 14:12:48 +0300

    add routes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,14 +2,17 @@ import Home from "./Home";
 import FavList from "./FavList";
 import PokeList from "./PokeList";
 import About from "./About";
+import { Route, Routes } from "react-router-dom";
 
 const Main = () => {
     return (
         <main>
-            <Home />
-            <FavList />
-            <PokeList />
-            <About />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/favlist" element={<FavList />} />
+                <Route path="/pokelist" element={<PokeList />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
         </main>
     );
 };
